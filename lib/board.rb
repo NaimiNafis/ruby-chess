6ebc9adb22e6a963e5c1083a5fab36b68728934c
NaimiNafis <126528516+NaimiNafis@users.noreply.github.com>
2024-05-25 16:16:32 +0900

    test(board): add test for get and set grid board refactor(board): refactor place_piece using operator overloading

--- lib/board.rb
+++ lib/board.rb
@@ -11,8 +11,14 @@ class Board
         ]
     end
 
-    def place(piece, location)
-      row, column = location
+    def []=(location, piece) #Operator Overloading
+        row, column = location
+        grid[row][column] = piece
+    end
+
+    def [](location)
+        row, column = location
+        grid[row][column]
     end
 
     def inbound?
@@ -20,10 +26,16 @@ class Board
     end
 
     def get_piece(location)
-        
+
     end
 end
+
+
 
 # place pieces
 # out of bounds
 # get a piece at spesific position
+
+# Operator Overloading. Exp: a = [1, 2, 3]
+# a.[](0)  => This is getter method for array
+# a.[]=(2, "lol") => This is setter method for array
---
+++ spec/board_spec.rb
@@ -0,0 +1,41 @@
+require './lib/board'
+
+describe Board do
+    describe "#[]=" do
+        it "get any pieces using location on grid" do
+            board = Board.new
+            location = [0, 0]
+            expected_result = [
+                ["Y", "X", nil, "X"],
+                ["X", "X", nil, "X"],
+                ["X", "X", nil, "X"],
+                ["X", "X", nil, "X"],
+            ]
+            board[location] = "Y"
+            expect(board.grid).to eql(expected_result)
+            expect(board[[1, 1]]).to eql("X")
+            expect(board[[1, 2]]).to eql(nil)
+        end
+
+        it "return 'X' on any location marked 'X'" do
+            board = Board.new
+            location = [1, 1]
+            expect(board[location]).to eql("X")
+        end
+
+        it "return nil on any location marked nil" do
+            board = Board.new
+            location = [1, 2]
+            expect(board[location]).to eql(nil)
+        end
+    end
+
+    describe "#[]" do
+        it "set any pieces using location on grid" do
+            board = Board.new
+            location = [0, 0]
+            board[location] = "Y"
+            expect(board[location]).to eql("Y")
+        end
+    end
+end
