d7a5a7d19d1fbf84f6f19b91c107322ad50beb9d
NaimiNafis <126528516+NaimiNafis@users.noreply.github.com>
2024-05-28 13:48:31 +0900

    feat(board_spec): add logic for moving and taking a piece - Add move_pieces tests refactor(board_spec): refactor Board.new - Refactor Board.new from each test for readabilty - Update all tests's describe texts for readabilty - Refactor so that all test will be starting to test 8x8 boards

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,30 +2,26 @@ require_relative '../lib/board'
 require_relative '../lib/chess_pieces/chess_pieces'
 
 describe Board do
+    let(:board) { Board.new }
+
     describe "#[]=" do
-        it "get any pieces using location on grid" do
-            board = Board.new(4)
+        it "sets a piece on the board at a specific location" do
             location = [0, 0]
-            expected_result = [
-                ["Y", nil, nil, nil],
-                [nil, nil, nil, nil],
-                [nil, nil, nil, nil],
-                [nil, nil, nil, nil],
-            ]
+            expected_result = Array.new(8) { Array.new(8) }
+            expected_result[0][0] = "Y"
             board[location] = "Y"
             expect(board.grid).to eql(expected_result)
         end
 
-        it "return nil on any location marked nil" do
-            board = Board.new
+        it "returns nil for any location marked nil" do
+            board[[1, 2]] = nil
             location = [1, 2]
             expect(board[location]).to eql(nil)
         end
     end
 
     describe "#[]" do
-        it "set any pieces using location on grid" do
-            board = Board.new
+        it "gets a piece from the board at a specific location" do
             location = [0, 0]
             board[location] = "Y"
             expect(board[location]).to eql("Y")
@@ -33,23 +29,19 @@ describe Board do
     end
 
     describe "#in_bounds?" do
-        it "return true if location in bounds" do
-            board = Board.new
+        it "returns true if the location is within bounds" do
             location = [0, 0]
             expect(board.in_bounds?(location)).to eql(true)
         end
 
-        it "return false if location out of bounds" do
-            board = Board.new
+        it "returns false if the location is out of bounds" do
             location = [-1, -2]
             expect(board.in_bounds?(location)).to eql(false)
         end
     end
 
     describe "#[]" do
-
-        it "get any pieces using location on grid" do
-            board = Board.new(4)
+        it "gets pieces using location on grid" do
             board[[0, 0]] = Rook.new(board, [0, 0], :white)
             board[[0, 1]] = Knight.new(board, [0, 1], :white)
             board[[0, 2]] = Bishop.new(board, [0, 2], :white)
@@ -60,10 +52,14 @@ describe Board do
             board[[1, 3]] = Pawn.new(board, [1, 3], :white)
             board[[3, 3]] = Pawn.new(board, [3, 3], :black)
             expected_result = [
-                ["♜", "♞", "♝", "♛"],
-                ["♚", "♟", "♟", "♟"],
-                [nil, nil, nil, nil],
-                [nil, nil, nil, "♙"],
+                ["♜", "♞", "♝", "♛", nil, nil, nil, nil],
+                ["♚", "♟", "♟", "♟", nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, "♙", nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil, nil]
             ]
             actual_result = board.grid.map { |row| row.map { |piece| piece&.to_s } }
             expect(actual_result).to eql(expected_result)
@@ -88,4 +84,55 @@ describe Board do
         end
     end
 
+    describe "#move_pieces" do
+        context "when moving a piece within its available moves" do
+            xit "moves the piece to the new position" do
+                black_pawn = Pawn.new(board, [1, 2], :black)
+                board[[1, 2]] = black_pawn
+                board.move_pieces([1, 2], [2, 2])
+                expect(board[[2, 2]]).to eq(black_pawn)
+                expect(board[[1, 2]]).to be_nil
+                expect(black_pawn.location).to eq([2, 2])
+            end
+        end
+
+        context "when trying to move a piece to an invalid position" do
+            xit "raises an error" do
+                black_pawn = Pawn.new(board, [1, 2], :black)
+                board[[1, 2]] = black_pawn
+                expect { board.move_pieces([1, 2], [3, 3]) }.to raise_error("Invalid move")
+            end
+        end
+
+        context "when capturing an enemy piece" do
+            xit "moves the piece and removes the enemy piece" do
+                black_pawn = Pawn.new(board, [1, 2], :black)
+                enemy_piece = Pawn.new(board, [2, 1], :white)
+                board[[1, 2]] = black_pawn
+                board[[2, 1]] = enemy_piece
+                board.move_pieces([1, 2], [2, 1])
+                expect(board[[2, 1]]).to eq(black_pawn)
+                expect(board[[1, 2]]).to be_nil
+                expect(black_pawn.location).to eq([2, 1])
+            end
+        end
+
+        context "when trying to move a piece out of bounds" do
+            xit "raises an error" do
+                black_pawn = Pawn.new(board, [1, 2], :black)
+                board[[1, 2]] = black_pawn
+                expect { board.move_pieces([1, 2], [-1, -1]) }.to raise_error("Invalid move")
+            end
+        end
+
+        context "when moving a piece to an occupied position by the same color" do
+            xit "raises an error" do
+                black_pawn = Pawn.new(board, [1, 2], :black)
+                blocking_piece = Pawn.new(board, [2, 2], :black)
+                board[[1, 2]] = black_pawn
+                board[[2, 2]] = blocking_piece
+                expect { board.move_pieces([1, 2], [2, 2]) }.to raise_error("Invalid move")
+            end
+        end
+    end
 end
