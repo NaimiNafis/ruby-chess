76a3e8fa2cc54b77f5e8663564cc989ee7ae20e9
NaimiNafis <126528516+NaimiNafis@users.noreply.github.com>
2024-05-29 17:36:07 +0900

    feat(player): add notation to coordinate translation in get_pos method
    - Updated the get_pos method to handle the translation from chess notation (e.g., 'e2') to board coordinates (e.g., [6, 4]).
    - Added helper methods notation_to_coordinate and letter_to_column for this conversion.

--- lib/player.rb
+++ lib/player.rb
@@ -3,20 +3,25 @@ class Player
     attr_reader :color
 
     def initialize(color)
-        @color = color
+      @color = color
     end
 
     def get_pos
-      gets.chomp.split("").map { |part| part.to_i }
+      input = gets.chomp
+      notation_to_coordinate(input)
     end
 
-    # get_pos to return an array of integer: [2, 2]
-    # we want to ask notation(i.e: e1), change it to coordinate
-    
-    # input: a1 = desired_output: [7,0]
-    # gets.chomp
-    # split = ["a", "1"] = column, row
-    # row: 8 - "1".to_i
-    # column: 'letter'.ord - 'a'.ord so, a = 0, b = 1, etc.
+    def notation_to_coordinate(notation)
+      letter, number = notation.split("")
+
+      column = letter_to_column(letter)
+      row = 8 - number.to_i
+
+      [row, column]
+    end
+
+    def letter_to_column(letter)
+      letter.ord - 'a'.ord
+    end
 
 end
