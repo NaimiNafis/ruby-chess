e72c0dbe8728d6b10e198699ddd020e306810f63
NaimiNafis <126528516+NaimiNafis@users.noreply.github.com>
2024-05-29 17:27:09 +0900

    feat(player): add todo list for input notation

--- lib/main.rb
+++ lib/main.rb
@@ -4,20 +4,6 @@ require_relative 'board'
 require_relative 'player'
 require_relative 'game'
 
-# TODO:
-#   add logic for "won"
-
-b = Board.new
-b[[6, 6]] = King.new(b, [6, 6], :white)
-b[[1, 1]] = King.new(b, [1, 1], :black)
-b[[2, 2]] = Queen.new(b, [2, 2], :white)
-
-# b = Board.start_chess
-# text_board = BoardRendererText.new(b)
-# piece = b[[1, 2]]
-# p piece.available_moves
-# puts text_board.render
-
-# b = Board.start_chess
+b = Board.start_chess
 game = Game.new(b, Player.new(:black), Player.new(:white), BoardRendererText)
 game.play
--- lib/player.rb
+++ lib/player.rb
@@ -7,6 +7,16 @@ class Player
     end
 
     def get_pos
-      gets.chomp.split(",").map { |part| part.to_i }
+      gets.chomp.split("").map { |part| part.to_i }
     end
+
+    # get_pos to return an array of integer: [2, 2]
+    # we want to ask notation(i.e: e1), change it to coordinate
+    
+    # input: a1 = desired_output: [7,0]
+    # gets.chomp
+    # split = ["a", "1"] = column, row
+    # row: 8 - "1".to_i
+    # column: 'letter'.ord - 'a'.ord so, a = 0, b = 1, etc.
+
 end
