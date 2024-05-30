d6f222caecfa72ea30a1ba315f4f7c124d6ba411
NaimiNafis <126528516+NaimiNafis@users.noreply.github.com>
2024-05-30 17:09:04 +0900

    refactor(game): improve readability and reduce redundancy
    - Extract repetitive checks and outputs into separate methods (display_turn_info, game_over?, display_winner)
    - Simplify take_turn by extracting select_start_position and select_end_position methods
    - Ensure exception handling for invalid moves is concise and clear

--- lib/game.rb
+++ lib/game.rb
@@ -9,91 +9,107 @@ require_relative './modules/display'
 
 class Game
   include NotationConverter
+  include SaveManager
   include Display
 
-  attr_reader :board, :player1, :player2, :renderer
+  attr_reader :board, :player1, :player2
   attr_accessor :current_player
 
   def initialize(board, player1, player2)
     @board = board
     @player1 = player1
     @player2 = player2
-    @current_player = @player1
+    @current_player = player1
   end
 
   def swap_player!
-    # Use self.current_player to call the setter method
     self.current_player = current_player == player1 ? player2 : player1
+  end
+
+  def save_game
+    SaveManager.save(self)
+  end
+
+  def self.load_game
+    SaveManager.load
   end
 
   def play
     loop do
       Display.render_board(board)
-      puts "It's #{current_player.color.capitalize}'s turn"
+      display_turn_info
 
-      if board.checkmate?(current_player.color)
-        puts "#{current_player.color.capitalize} is in checkmate!"
+      if game_over?
+        display_winner
         break
-      elsif board.in_check?(current_player.color)
-        puts "#{current_player.color.capitalize} is in check!"
       end
 
       take_turn
-
-      if board.checkmate?(current_player.color)
-        Display.render_board(board)
-        puts "#{current_player.color.capitalize} is in checkmate!"
-        break
-      elsif board.in_check?(current_player.color)
-        puts "#{current_player.color.capitalize} is in check!"
-      end
-
       swap_player!
     end
 
     Display.render_board(board)
-    swap_player!
     puts "\nGame Over! The winner is: #{current_player.color.capitalize}!\n"
   end
 
-  def take_turn
-    start_pos = nil
+  private
 
+  def display_turn_info
+    puts "It's #{current_player.color.capitalize}'s turn"
+    if board.checkmate?(current_player.color)
+      puts "#{current_player.color.capitalize} is in checkmate!"
+    elsif board.in_check?(current_player.color)
+      puts "#{current_player.color.capitalize} is in check!"
+    end
+  end
+
+  def game_over?
+    board.checkmate?(current_player.color)
+  end
+
+  def display_winner
+    swap_player!
+    puts "#{current_player.color.capitalize} is in checkmate!"
+  end
+
+  def take_turn
+    start_pos = select_start_position
+    end_pos = select_end_position(start_pos)
+
+    board.move_piece(start_pos, end_pos)
+    rescue InvalidMoveError => e
+      puts e.message
+      retry
+  end
+
+  def select_start_position
     loop do
       puts 'Select a piece to move: '
       start_pos = current_player.get_pos
 
-      if board[start_pos].nil?
+      piece = board[start_pos]
+      if piece.nil?
         puts 'There is no piece at the selected position.'
-        next
-      end
-
-      if board[start_pos].color != current_player.color
+      elsif piece.color != current_player.color
         puts "Did not select a #{current_player.color} piece."
-        next
-      end
-
-      if board[start_pos].available_moves.empty?
+      elsif piece.available_moves.empty?
         puts 'There are no available moves for this piece.'
-        next
+      else
+        available_moves = piece.available_moves.map { |move| coordinate_to_notation(move) }
+        puts "Available moves: #{available_moves.join(', ')}"
+        return start_pos
       end
-
-      available_moves = board[start_pos].available_moves.map { |move| coordinate_to_notation(move) }
-      puts "Available moves: #{available_moves.join(', ')}"
-
-      break
     end
+  end
 
+  def select_end_position(start_pos)
     loop do
       puts 'Select a position to move: '
       end_pos = current_player.get_pos
 
-      begin
-        board.move_piece(start_pos, end_pos)
-        break
-      rescue InvalidMoveError => e
-        puts e.message
-      end
+      return end_pos if board.valid_move?(start_pos, end_pos)
+      
+      puts 'Invalid move, try again.'
     end
   end
 end
